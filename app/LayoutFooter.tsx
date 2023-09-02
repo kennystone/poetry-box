e2b3ca3c4035497cc238fb92d30f20d65a4ae031
Kenny Stone <kenny.stone@hey.com>
2023-09-02 09:31:16 -0700

    duh

--- app/LayoutFooter.tsx
+++ app/LayoutFooter.tsx
@@ -2,27 +2,6 @@ import { BookMania } from './fonts/Bookmania'
 import Image from 'next/image'
 import uplandsImg from './images/uplands.svg'
 import UplandsSvg from "./images/UplandsSvg"
-import resolveConfig from 'tailwindcss/resolveConfig'
-import { content, theme } from '../tailwind.config.js'
-
-
-const getRegal = () => {
-    const fullConfig = resolveConfig({
-        content,
-        theme,
-    })
-    const colors = fullConfig?.theme?.colors
-
-    let regal = "black" // fallback color if undefined
-
-    if (colors !== undefined) {
-        if (typeof colors.regal !== 'string' && colors.regal !== undefined) {
-            if (typeof colors.regal["500"] === 'string')
-                regal = colors.regal["500"]
-        }
-    }
-    return regal
-}
 
 const LayoutFooter = () => {
     const unused1 = <UplandsSvg width="80" />
@@ -34,7 +13,7 @@ const LayoutFooter = () => {
         </div>
         <div className="flex items-center justify-center">
             <div className="px-7">
-                <UplandsSvg className="" width="120" stroke={getRegal()} />
+                <UplandsSvg className="stroke-regal-500" width="120" />
             </div>
         </div>
         <h3 className="text-center text-regal-500">A project by K+M</h3>
